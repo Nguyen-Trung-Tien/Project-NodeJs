8390dfc78633172324a6a6f8d5d5fcd92df9af2d
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-06-22 21:22:00 +0700

    learn connect msql

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,14 @@
-let getHomePage = (req, res) => {
-  return res.send("hello world controller");
-};
+import db from "../models/index";
 
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 module.exports = { getHomePage };
