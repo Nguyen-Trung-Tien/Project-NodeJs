b9b3735e73d916909e98122f6e698290fa334d4e
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-07-05 14:43:15 +0700

    finish handle login

--- Backend-NodeJS/src/server.js
+++ Backend-NodeJS/src/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
+import cors from "cors";
 import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 let app = express();
-
+app.use(cors({ credentials: true, origin: true }));
 // config app
 
 app.use(bodyParser.json());
--- Frontend/src/containers/Auth/Login.js
+++ Frontend/src/containers/Auth/Login.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
-// import * as actions from "../store/actions";
 import * as actions from "../../store/actions";
-
 import "./Login.scss";
 import { FormattedMessage } from "react-intl";
+import { handleLoginApi } from "../../services/userService";
 
 class Login extends Component {
   constructor(props) {
@@ -14,6 +13,7 @@ class Login extends Component {
       username: "",
       password: "",
       isShowPassword: false,
+      errMessage: "",
     };
   }
 
@@ -28,14 +28,30 @@ class Login extends Component {
     });
   };
 
-  handleLogin = () => {
-    console.log(
-      "username",
-      this.state.username,
-      "password",
-      this.state.password
-    );
-    console.log("all state", this.state);
+  handleLogin = async () => {
+    this.setState({
+      errMessage: "",
+    });
+    try {
+      let data = await handleLoginApi(this.state.username, this.state.password);
+      if (data && data.errCode !== 0) {
+        this.setState({
+          errMessage: data.message,
+        });
+      }
+      if (data && data.errCode === 0) {
+        this.props.userLoginSuccess(data.user);
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.data) {
+          this.setState({
+            errMessage: error.response.data.message,
+          });
+        }
+      }
+      console.log(">>>", error.response);
+    }
   };
 
   handleShowPassword = () => {
@@ -83,11 +99,15 @@ class Login extends Component {
                   ></i>
                 </span>
               </div>
-
+              <div className="col-12" style={{ color: "red" }}>
+                {this.state.errMessage}
+              </div>
               <div className="col-12 ">
                 <button
                   className="btn-login"
-                  onClick={() => this.handleLogin()}
+                  onClick={() => {
+                    this.handleLogin();
+                  }}
                 >
                   Login
                 </button>
@@ -119,9 +139,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     navigate: (path) => dispatch(push(path)),
-    adminLoginSuccess: (adminInfo) =>
-      dispatch(actions.adminLoginSuccess(adminInfo)),
-    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    // userLoginFail: () => dispatch(actions.userLoginFail()),
+    userLoginSuccess: (userInfo) =>
+      dispatch(actions.userLoginSuccess(userInfo)),
   };
 };
 
