c9c0f1e5273eded798589f61d8e734b7202a4b74
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-07-07 21:15:44 +0700

    learn view CRUD

---
+++ Frontend-ReactJS/src/containers/System/UserManage.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManager.scss";
+import { getAllUsers } from "../../services/userService";
+
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
+  async componentDidMount() {
+    let response = await getAllUsers("All");
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
+    }
+  }
+
+  render() {
+    console.log(">>state", this.state);
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="user-container">
+        <div className="title text-center"> Manager User</div>
+        <div className="users-table mt-4 mx-1">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>Firs name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Action</th>
+            </tr>
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firsName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i className="fas fa-pencil-alt"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i className="fas fa-trash"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
