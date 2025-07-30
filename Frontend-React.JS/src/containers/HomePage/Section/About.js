e247a000e7c702ad0ed394bbd7c1f0e28d029cb7
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-07-30 12:44:20 +0700

    fix redux

--- Frontend-React.JS/src/containers/HomePage/Section/About.js
+++ Frontend-React.JS/src/containers/HomePage/Section/About.js
@@ -15,10 +15,10 @@ class About extends Component {
               height="400px"
               src="https://www.youtube.com/embed/6boYRkRcRTs"
               title="Thủ tướng Malaysia nói về tình hình mới nhất giữa Campuchia - Thái Lan, LHQ họp khẩn | Tin tức"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
             ></iframe>
           </div>
           <div className="content-right">
