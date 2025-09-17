48b21aba6ecf6d4c3d1edc617a47b3f4f395ad54
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-09-17 14:51:12 +0700

    customize config

--- Backend-Node.JS/build/models/index.js
+++ Backend-Node.JS/build/models/index.js
@@ -1,29 +1,29 @@
 "use strict";
+
 require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || "development";
 // const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
-
-let sequelize;
-
-const customizeConfig = {
+var db = {};
+var sequelize;
+var customizeConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DB_DIALECT,
   logging: false,
-  query: { raw: true },
+  query: {
+    raw: true,
+  },
   timezone: "+07:00",
 };
-
 sequelize = new Sequelize(
   process.env.DB_DATABASE_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
-  customizeConfig()
+  customizeConfig
 );
 
 // if (config.use_env_variable) {
@@ -38,26 +38,23 @@ sequelize = new Sequelize(
 // }
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
+  .filter(function (file) {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
     );
   })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
+  .forEach(function (file) {
+    var model = require(path.join(__dirname, file))(
       sequelize,
       Sequelize.DataTypes
     );
     db[model.name] = model;
   });
-
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(function (modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
